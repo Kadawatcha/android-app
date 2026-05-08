5650d3ca51095c6340bef378ce70eabb762a9a00
Kadawatcha <163724568+Kadawatcha@users.noreply.github.com>
2026-05-08 11:58:07 +0200

    Implementation of CounterScreen and CounterViewModel

--- app/src/main/java/com/example/kad/MainActivity.kt
+++ app/src/main/java/com/example/kad/MainActivity.kt
@@ -7,23 +7,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.kad.ui.CounterScreen // Import de ton nouvel écran
-import com.example.kad.viewmodel.CounterViewModel // Import du cerveau
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.kad.ui.CounterScreen
 import com.example.kad.ui.theme.KadTheme
+import com.example.kad.viewmodel.CounterViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        // On prépare le cerveau
-        val viewModel = CounterViewModel()
-
         setContent {
+            // Utilisation de la fonction viewModel() pour récupérer l'instance du ViewModel
+            val myViewModel: CounterViewModel = viewModel()
+
             KadTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    // On appelle l'artiste (l'écran) et on lui donne les outils
-                    CounterScreen(viewModel = viewModel, padding = innerPadding)
+                    // On passe le viewModel et les marges du Scaffold à ton écran
+                    CounterScreen(
+                        viewModel = myViewModel,
+                        padding = innerPadding
+                    )
                 }
             }
         }
